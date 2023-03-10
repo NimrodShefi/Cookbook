3b64a5a12c96570dfb31f15fb7f849246c1d085c
Nimrod Shefi <nimrodshefi01@gmail.com>
2023-03-10 16:40:15 +0000

    adding the home page with a navbar and a base html page that will be used in all pages of the cookbook

--- app/app.py
+++ app/app.py
@@ -1,7 +1,11 @@
-from flask import Flask, render_template 
+from flask import Flask, render_template, redirect
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hello"
+    return redirect("/home")
+
+@app.route('/home')
+def home():
+    return render_template("home.html")
