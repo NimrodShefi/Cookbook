044161e73668f480dcbf9aee8291f58360bbe266
Nimrod Shefi <nimrodshefi01@gmail.com>
2023-04-19 20:15:16 +0100

    should the db not exist anymore for some reason, the error handler will run the function needed to recreate it

--- cookbook/errors/handlers.py
+++ cookbook/errors/handlers.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect
+from sqlalchemy import exc
 
 errors = Blueprint("errors", __name__)
 
@@ -19,3 +20,7 @@ def error_500(error):
     return render_template("errors/error.html", 
                             error_type="Something went wrong (500)", 
                             error_msg="We're experiencing some trouble on our end. Please try again in the near future"), 500
+
+@errors.app_errorhandler(exc.OperationalError)
+def error_exc_OperationalError(error):
+    return redirect("/")
