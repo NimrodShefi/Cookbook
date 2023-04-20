8e965a135c4421cb9afa65449f27655ef7e15b66
Nimrod Shefi <nimrodshefi01@gmail.com>
2023-04-20 16:34:23 +0100

    trying something else so that the heroku server wont crush

--- cookbook/config.py
+++ cookbook/config.py
@@ -3,7 +3,7 @@ class Config:
     # Secret Key
     SECRET_KEY = "mysecretkey"
     # Add Database
-    SQLALCHEMY_DATABASE_URI = "postgres://emmnolrfevgawp:4c13aca4ca03907b8df983bfaf70bb3dd3e737d6a995903c37a5afc045f2d98b@ec2-52-215-68-14.eu-west-1.compute.amazonaws.com:5432/d6bh4oucanhkha"
+    SQLALCHEMY_DATABASE_URI = "postgresql://emmnolrfevgawp:4c13aca4ca03907b8df983bfaf70bb3dd3e737d6a995903c37a5afc045f2d98b@ec2-52-215-68-14.eu-west-1.compute.amazonaws.com:5432/d6bh4oucanhkha"
     # SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:123654@localhost/my_cookbook"
     MAIL_SERVER = 'smtp.gmail.com'
     MAIL_POST = 587
