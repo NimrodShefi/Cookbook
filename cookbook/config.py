66ef1f2e26c9cb974a60144a695c465e425a55cf
Nimrod Shefi <nimrodshefi01@gmail.com>
2023-04-20 15:57:20 +0100

    removing the local constants file and preparing the code for Heroku

--- cookbook/config.py
+++ cookbook/config.py
@@ -1,14 +1,12 @@
-# This is a locally stored file that will not get uploaded to github
-from cookbook.constants import SECRET_KEY, SQLALCHEMY_DATABASE_URI, MAIL_USERNAME, MAIL_PASSWORD
 
 class Config:
     # Secret Key
-    SECRET_KEY = SECRET_KEY
+    SECRET_KEY = "mysecretkey"
     # Add Database
-    SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI
-
+    SQLALCHEMY_DATABASE_URI = "postgres://emmnolrfevgawp:4c13aca4ca03907b8df983bfaf70bb3dd3e737d6a995903c37a5afc045f2d98b@ec2-52-215-68-14.eu-west-1.compute.amazonaws.com:5432/d6bh4oucanhkha"
+    # SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:123654@localhost/my_cookbook"
     MAIL_SERVER = 'smtp.gmail.com'
     MAIL_POST = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = MAIL_USERNAME
-    MAIL_PASSWORD = MAIL_PASSWORD
+    MAIL_USERNAME = "namerspam@gmail.com"
+    MAIL_PASSWORD = "gidtewluauzjsuqi"
