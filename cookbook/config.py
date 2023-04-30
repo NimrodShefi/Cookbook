6998639c87875751952e3081801425808fba0f26
Nimrod Shefi <nimrodshefi01@gmail.com>
2023-04-30 12:59:15 +0100

    trying to fix the db URI part 2

--- cookbook/config.py
+++ cookbook/config.py
@@ -2,7 +2,7 @@ class Config:
     # Secret Key
     SECRET_KEY = "mysecretkey"
     # Add Database
-    SQLALCHEMY_DATABASE_URI = "postgresql://bbszrmeoacoinc:f431c1680330f0102af84b9f34ec4aeef9bb9b12e4c09d04867357871fa906cc@ec2-3-210-173-88.compute-1.amazonaws.com:5432/d4c4mslcts5ecg"
+    SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://bbszrmeoacoinc:f431c1680330f0102af84b9f34ec4aeef9bb9b12e4c09d04867357871fa906cc@ec2-3-210-173-88.compute-1.amazonaws.com:5432/d4c4mslcts5ecg"
     # SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:123654@localhost/my_cookbook"
     MAIL_SERVER = 'smtp.gmail.com'
     MAIL_POST = 587
